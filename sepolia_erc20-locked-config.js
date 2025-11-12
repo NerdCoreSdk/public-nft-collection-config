32b27879099c266d60182dbbca684279b4e77a0b
jedster189 <jedster189@gmail.com>
2025-11-12 14:28:48 -0700

    feat(config): Update LACEX Locked Test Collection address and remove Test Brand configuration

--- sepolia_erc20-locked-config.js
+++ sepolia_erc20-locked-config.js
@@ -10,7 +10,7 @@ module.exports = {
         {
           "name": "LACEX Locked Test Collection",
           "symbol": "LACEXSEP",
-          "address": "0xE53d4B758c8946a805eFda1539Bf68006239B84B",
+          "address": "0x5ddCcBF0b5bfc1F5Ce2a1a9Be2DBaa3823a543B3",
           "baseURI": "https://bafybeigjrida3jsfen7d4ujcodegvxbl2f6c4cy4e2twthmp4yxfi573om.ipfs.w3s.link/",
           "maxSupply": 100,
           "royaltiesPercentage": 500,
@@ -18,27 +18,6 @@ module.exports = {
           "rewardTokenAddress": "0x33704C90cAf0B01eD7b5d1858BC0c07ce28F0734",
           "rewardTokenDecimals": 8,
           "initialRewardPoolSize": "100"
-        }
-      ]
-    },
-    {
-      "brandName": "Test Brand",
-      "brandOwner": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-      "rewardTokenAddress": "0x378CE74b4b4472055429664733892F958c9FC9B7",
-      "rewardTokenSymbol": "TEST",
-      "rewardTokenDecimals": 18,
-      "collections": [
-        {
-          "name": "Test HODL Basic",
-          "symbol": "tHODL-1",
-          "address": "0x13D1792880dd9CAa71498576054e18441dCaEeb0",
-          "baseURI": "https://testnet-metadata.example.com/basic/",
-          "maxSupply": 100,
-          "royaltiesPercentage": 500,
-          "royaltiesReceiver": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-          "rewardTokenAddress": "0x378CE74b4b4472055429664733892F958c9FC9B7",
-          "rewardTokenDecimals": 18,
-          "initialRewardPoolSize": "10000"
         }
       ]
     }
