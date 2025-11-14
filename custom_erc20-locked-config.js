48565a01622fb1f51f0ac8c1c6bb96a7680fd4fe
jedster189 <jedster189@gmail.com>
2025-11-14 16:43:55 -0700

    feat(config): Refactor brand configuration to streamline account roles and NFT collections

--- custom_erc20-locked-config.js
+++ custom_erc20-locked-config.js
@@ -2,63 +2,57 @@ module.exports = {
   "brands": [
     {
       "brandName": "Test Brand",
-      "brandOwner": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
+      "accounts": [
+        {
+          "account": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        },
+        {
+          "account": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+          "roles": [
+            "MINTER_ROLE"
+          ]
+        },
+        {
+          "account": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+          "roles": [
+            "MINTER_ROLE"
+          ]
+        }
+      ],
       "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
       "rewardTokenSymbol": "TEST",
       "rewardTokenDecimals": 18,
       "collections": [
         {
           "name": "Test HODL Reward NFT 1",
+          "tiers": [
+            {
+              "name": "Tier 1",
+              "lockAmountUSD": 1000,
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 300
+            },
+            {
+              "name": "Tier 2",
+              "lockAmountUSD": 3000,
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 600
+            },
+            {
+              "name": "Tier 3",
+              "lockAmountUSD": 5000,
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 86400
+            }
+          ],
           "symbol": "tHODL-1",
-          "address": "0x51b60CC843aE7AbBC2F387c7C281d42D0d9ed648",
+          "address": "0x52b3c2AbE4C56316F9db497E100e2cd3B24999ee",
           "baseURI": "https://test-metadata.example.com/hodl1/",
           "maxSupply": 50,
           "royaltiesPercentage": 500,
-          "royaltiesReceiver": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-          "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
-          "rewardTokenDecimals": 18,
-          "initialRewardPoolSize": "0"
-        },
-        {
-          "name": "Test HODL Reward NFT 2",
-          "symbol": "tHODL-2",
-          "address": "0x512B4BD1324b0b9180Cc69DE7C11350CdF39c3b2",
-          "baseURI": "https://test-metadata.example.com/hodl2/",
-          "maxSupply": 30,
-          "royaltiesPercentage": 750,
-          "royaltiesReceiver": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-          "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
-          "rewardTokenDecimals": 18,
-          "initialRewardPoolSize": "0"
-        },
-        {
-          "name": "Test HODL Reward NFT 3",
-          "symbol": "tHODL-3",
-          "address": "0x283339718a59b5B0A6a19D9a76F885f49c2Cc873",
-          "baseURI": "https://test-metadata.example.com/hodl3/",
-          "maxSupply": 300,
-          "royaltiesPercentage": 0,
-          "royaltiesReceiver": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-          "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
-          "rewardTokenDecimals": 18,
-          "initialRewardPoolSize": "0"
-        }
-      ]
-    },
-    {
-      "brandName": "Brand2",
-      "brandOwner": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
-      "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
-      "rewardTokenSymbol": "TEST",
-      "rewardTokenDecimals": 18,
-      "collections": [
-        {
-          "name": "Brand2 Premium NFT Collection",
-          "symbol": "B2-PREM",
-          "address": "0x1EBef3c190821b32C49052B8f1E055592877Bb86",
-          "baseURI": "https://brand2-metadata.example.com/premium/",
-          "maxSupply": 100,
-          "royaltiesPercentage": 750,
           "royaltiesReceiver": "0x2201374e803Ec5Ca1dCB6550673669b42D9A5B70",
           "rewardTokenAddress": "0xAAF62976ee828b91f06615f2007f995d2400699c",
           "rewardTokenDecimals": 18,
