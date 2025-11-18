7183d51c4824714d88fc4d9f22c29c03477d801e
jedster189 <jedster189@gmail.com>
2025-11-18 11:48:59 -0700

    feat(config): Add initial ERC20 locked configuration for WIN Test Locked Collection

---
+++ ethereum_erc20-locked-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  "googleSheetsId": "1eA4wpaN_ecfGUr51qiZodEwRe8QyZ7E4Vuxuxmj99Gs",
+  "brands": [
+    {
+      "brandName": "Connect",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0xb10CB07CA2CDac77FbB5707f6690301f9d036F45",
+      "rewardTokenSymbol": "WIN",
+      "rewardTokenDecimals": 18,
+      "collections": [
+        {
+          "name": "WIN Test Locked Collection",
+          "symbol": "WINTLC",
+          "address": "0x97Eb78AfC61A54858cE21c50Fe0C5A8D3Ed623CD",
+          "baseURI": "https://bafybeidsfpzfdqd2wfoc3t2sa3oqyvbazmv4bmzizcokdbk7zbuce2c4ce.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0xb10CB07CA2CDac77FbB5707f6690301f9d036F45",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "5000 Pro Match NFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 300
+            },
+            {
+              "name": "2000 Pro Match NFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 3600
+            },
+            {
+              "name": "1000 Pro Match NFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 86400
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
