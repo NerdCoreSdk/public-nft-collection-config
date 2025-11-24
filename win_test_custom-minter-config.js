48249d516106d23a523b559b1cb7d43ed0f0be6e
jedster189 <jedster189@gmail.com>
2025-11-23 22:39:00 -0700

    feat(config): Add Google Sheets ID and enhance NFT configurations with tiers and contract types

--- win_test_custom-minter-config.js
+++ win_test_custom-minter-config.js
@@ -7,6 +7,7 @@ module.exports = {
     nftMintVoucherAddress: "0xF5E0bC0fa749754a22CB2fb7FE01602a561b619B",
     coreAPI: "https://api.dev.connectunited.com/",
     chainID: 2666328,
+    "googleSheetsId": "1ge9GoJHmQlHrTvm-6Ay6V7R6Zhh0q8bgTBq9IcxxyLM",
     admins: [
         { address: "0x58f1a71C79FB6685a75Ada2Cd5717Bb2Fb70515F" }, // Manual Deployment Address
         { address: "0xAdBee901963233232bd6C93d0F6b0C3e9c5B499C" }, // Disbursement Minting Address
@@ -17,26 +18,114 @@ module.exports = {
       "name": "WIN DEV Test NFT Collection",
       "symbol": "WDTNFT",
       "baseURI": "https://bafybeienfi3u6c6dlovmustmvztdjdqvrahjgw7ichblr67jthodtez65y.ipfs.w3s.link/",
-      "maxSupply": 500
+      "maxSupply": 500,
+      "tiers": [
+        {
+          "startTokenId": 1,
+          "endTokenId": 500
+        }
+      ],
+      "contractType": "CouponNFT"
     },
     {
       "address": "0x100f9D711850f058ABbac216A1c76b9B2Df7824d",
       "name": "WIN DEV Test NFT Collection",
       "symbol": "WDTNFT",
       "baseURI": "https://bafybeienfi3u6c6dlovmustmvztdjdqvrahjgw7ichblr67jthodtez65y.ipfs.w3s.link/",
-      "maxSupply": 500
+      "maxSupply": 500,
+      "tiers": [
+        {
+          "startTokenId": 1,
+          "endTokenId": 500
+        }
+      ],
+      "contractType": "CouponNFT"
     },
     {
       "address": "0xAA346A5fa0860132625Ed2E6F88D0C9A964ef7Ec",
       "name": "Win-Testnet-Boost-1-Day",
       "symbol": "WTDNFT",
-      "maxSupply": 500
+      "maxSupply": 500,
+      "tiers": [
+        {
+          "startTokenId": 1,
+          "endTokenId": 500
+        }
+      ],
+      "contractType": "CouponNFT"
     },
     {
       "address": "0x3a589074BaA9B3E368064A7Ca6ABc87f2eD749fb",
       "name": "WIN Legendary Node NFT Boost (Collection 1)",
       "symbol": "WINNBL1",
-      "maxSupply": 5000
+      "maxSupply": 5000,
+      "tiers": [
+        {
+          "startTokenId": 1,
+          "endTokenId": 5000
+        }
+      ],
+      "contractType": "CouponNFT"
     },
-  ]
+    {
+      "address": "",
+      "name": "Test Reward Token",
+      "symbol": "TRT",
+      "contractType": "RewardLockedNFT",
+      "baseURI": "https://w3s.link/ipfs/bafybeif5eganbpbl3frhymyn54csnzn5ickqpvn6tgszp6spaby5dqrbie/",
+      "maxSupply": 300,
+      "tiers": [
+        {
+          "startTokenId": 1,
+          "endTokenId": 100
+        },
+        {
+          "startTokenId": 101,
+          "endTokenId": 200
+        },
+        {
+          "startTokenId": 201,
+          "endTokenId": 300
+        }
+      ]
+    },
+  ],
+  "contractTypeRegistry": {
+    "CouponNFT": {
+      "mintSelector": "mint(address,uint256)",
+      "batchSelector": "mintMultiple(address[],uint256[])",
+      "batchSupported": true,
+      "parameters": {
+        "mint": [
+          "toAddress",
+          "tokenId"
+        ],
+        "mintMultiple": [
+          "toAddresses",
+          "tokenIds"
+        ]
+      }
+    },
+    "RewardLockedNFT": {
+      "mintSelector": "mintWithLock(address,uint256,uint256,uint256,uint256)",
+      "batchSelector": "mintMultipleWithLock(address[],uint256[],uint256[],uint256[],uint256[])",
+      "batchSupported": true,
+      "parameters": {
+        "mintWithLock": [
+          "toAddress",
+          "tokenId",
+          "lockAmount",
+          "rewardAmount",
+          "unlockTime"
+        ],
+        "mintMultipleWithLock": [
+          "toAddresses",
+          "tokenIds",
+          "lockAmounts",
+          "rewardAmounts",
+          "unlockTimes"
+        ]
+      }
+    }
+  }
 };
