3b51ee13dce760dd38f9ca3a118097c32a02c182
jedster189 <jedster189@gmail.com>
2025-11-24 18:32:49 -0700

    feat(config): Update rewardTokenDecimals to 8 and add new Elevate Match NFT configurations

--- ethereum_erc20-locked-config.js
+++ ethereum_erc20-locked-config.js
@@ -13,7 +13,7 @@ module.exports = {
       ],
       "rewardTokenAddress": "0xb10CB07CA2CDac77FbB5707f6690301f9d036F45",
       "rewardTokenSymbol": "WIN",
-      "rewardTokenDecimals": 18,
+      "rewardTokenDecimals": 8,
       "collections": [
         {
           "name": "WIN Test Locked Collection",
@@ -41,6 +41,470 @@ module.exports = {
               "name": "1000 Pro Match NFT",
               "baseRewardAmount": 0,
               "lockDurationSeconds": 86400
+            }
+          ]
+        },
+        {
+          "name": "$ Elevate Match NFT (WIN)",
+          "symbol": "$EMNWIN",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0xb10CB07CA2CDac77FbB5707f6690301f9d036F45",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Element",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0x600d601d8b9eb5de5ac90fefc68d0d08801bfd3f",
+      "rewardTokenSymbol": "ELMT",
+      "rewardTokenDecimals": 8,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Element)",
+          "symbol": "$EMNELMT",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0x600d601d8b9eb5de5ac90fefc68d0d08801bfd3f",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Elevate",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0x888888888887715FB9D9F84175Af9e6cCE46807e",
+      "rewardTokenSymbol": "ELEVATE",
+      "rewardTokenDecimals": 8,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Elevate)",
+          "symbol": "$EMNELEVATE",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0x888888888887715FB9D9F84175Af9e6cCE46807e",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Galvan",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0xf7e945fce8f19302aacc7e1418b0a0bdef89327b",
+      "rewardTokenSymbol": "IZE",
+      "rewardTokenDecimals": 8,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Galvan)",
+          "symbol": "$EMNIZE",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0xf7e945fce8f19302aacc7e1418b0a0bdef89327b",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Grow",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0x1223334444A7466fBf985B14E1F4EdAf3883bCa6",
+      "rewardTokenSymbol": "GROW",
+      "rewardTokenDecimals": 18,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Grow)",
+          "symbol": "$EMNGROW",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0x1223334444A7466fBf985B14E1F4EdAf3883bCa6",
+          "rewardTokenDecimals": 18,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Rally",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0x19640000000ba88d36206BeB10D0e86011C8d08c",
+      "rewardTokenSymbol": "RALLY",
+      "rewardTokenDecimals": 18,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Rally)",
+          "symbol": "$EMNRALLY",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0x19640000000ba88d36206BeB10D0e86011C8d08c",
+          "rewardTokenDecimals": 18,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Green",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0xb2089A7069861C8D90c8dA3aaCAB8e9188C0C531",
+      "rewardTokenSymbol": "GREEN",
+      "rewardTokenDecimals": 8,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Green)",
+          "symbol": "$EMNGREEN",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0xb2089A7069861C8D90c8dA3aaCAB8e9188C0C531",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "brandName": "Switch",
+      "accounts": [
+        {
+          "account": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "roles": [
+            "DEFAULT_ADMIN_ROLE"
+          ]
+        }
+      ],
+      "rewardTokenAddress": "0xb10cc888cb2cce7036f4c7ecad8a57da16161338",
+      "rewardTokenSymbol": "SWITCH",
+      "rewardTokenDecimals": 8,
+      "collections": [
+        {
+          "name": "$ Elevate Match NFT (Switch)",
+          "symbol": "$EMNSWITCH",
+          "address": "",
+          "baseURI": "https://{}.ipfs.w3s.link/",
+          "maxSupply": 0,
+          "royaltiesPercentage": 0,
+          "royaltiesReceiver": "0x6383d110aca68357a86fcdc05311d989913621f1",
+          "rewardTokenAddress": "0xb10cc888cb2cce7036f4c7ecad8a57da16161338",
+          "rewardTokenDecimals": 8,
+          "initialRewardPoolSize": "0",
+          "tiers": [
+            {
+              "name": "25MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "50MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "100MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "1000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "2000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
+            },
+            {
+              "name": "5000MatchNFT",
+              "baseRewardAmount": 0,
+              "lockDurationSeconds": 15724800
             }
           ]
         }
